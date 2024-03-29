24a859e27f4cbd2e00aa3abeb782f4669a2bc4a7
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-03-30 02:52:54 +0800

    Add which key.

--- lua/options.lua
+++ lua/options.lua
@@ -1,13 +1,14 @@
 -- general wants
-vim.opt.clipboard="unnamedplus"
-vim.opt.number=true
-vim.opt.relativenumber=true
-vim.opt.scrolloff=999
-vim.opt.ignorecase=true
-vim.opt.smartcase=true
-vim.opt.si=true
-vim.opt.splitbelow=true
-vim.opt.splitright=true
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
-
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 999
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.si = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shell = "powershell.exe"
+vim.opt.wrap = false
