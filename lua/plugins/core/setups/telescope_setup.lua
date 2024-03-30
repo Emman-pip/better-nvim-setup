2e029af9696f3ba5e451b6c5a363f4757a6d1e3b
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-03-30 11:05:18 +0800

    Add new keymaps.
    Keymaps for buffer switching.

--- lua/plugins/core/setups/telescope_setup.lua
+++ lua/plugins/core/setups/telescope_setup.lua
@@ -3,3 +3,8 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- unrelated keymaps here
+vim.keymap.set("n", "<Leader>n", ":bnext<cr>", {})
+vim.keymap.set("n", "<Leader>b", ":bprevious<cr>", {})
+vim.keymap.set("n", "<Leader>d", ":bdelete<cr>", {})
